a0955e253ba5367a121d3616384de1ef4c50d1e8
KeeganM22 <119663094+KeeganM22@users.noreply.github.com>
2023-02-20 17:15:01 -0700

    added all files from lab 4

---
+++ BlazorHybridApp/Data/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHybridApp.Data
+{
+    class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool isDone { get; set; } = false;
+    }
+}
